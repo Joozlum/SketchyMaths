19c9d03d78a67c97b2e5e6e8704732f46672e00f
Joozlum <Ouzelum@gmail.com>
2019-08-02 18:49:00 -0400

    Added labels for text inputs and added generic setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SketchyMaths',
+    version='0.9.0',
+    packages=['tests', 'sketchymaths'],
+    package_dir={'': 'sketchymaths'},
+    url='https://github.com/Joozlum/SketchyMaths',
+    license='',
+    author='Joozlum',
+    author_email='Ouzelum@gmail.com',
+    description='Mind-Mapping for Maths'
+)
