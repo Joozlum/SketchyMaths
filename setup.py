3e8674a3695804d593c9514ada2ff523b8a751f1
Joozlum <ouzelum@gmail.com>
2021-04-02 14:27:27 -0400

    Added Tutorial in Examples

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name='SketchyMaths',
-    version='0.9.0',
+    version='0.1.0',
     packages=setuptools.find_packages(),
     package_data={'SketchyExamples': ['data/SketchyExamples.bak', 'data/SketchyExamples.dat', 'SketchyExamples.dir']},
     entry_point={'console_scripts': ['SketchyMaths:main']},
     url='https://github.com/Joozlum/SketchyMaths',
-    license='',
+    license='MIT',
     author='Joozlum',
     author_email='Ouzelum@gmail.com',
     description='Mind-Mapping for Maths'
